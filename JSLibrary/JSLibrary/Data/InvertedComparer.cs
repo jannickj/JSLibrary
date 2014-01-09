42370faf254199aa4486e66220eac5134be0b18b
jannick <jannickboese@msn.com>
2014-01-09 15:51:55 +0100

    Added inverted comparer

---
+++ JSLibrary/JSLibrary/Data/InvertedComparer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JSLibrary.Data
+{
+	public class InvertedComparer<T> : IComparer<T> where T : IComparable<T>
+	{
+		public int Compare(T x, T y)
+		{
+			return y.CompareTo(x);
+		}
+	}
+}
