56458ce6e467d0725c09996729f166c3c1d5ed53
And <andreas.spejder@gmail.com>
2014-01-17 14:23:41 +0100

    SortableList added. Fully tested

---
+++ JSLibrary/JSLibrary/Data/SortableList.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JSLibrary.Data
+{
+    public class SortableList<T> : IEnumerable<T> where T : IComparable
+    {
+        private SortedList<int, T[]> sl = new SortedList<int, T[]>();
+        public int Count { get; private set; }
+        
+        public void Clear()
+        {
+            lock (sl)
+            {
+                Count = 0;
+                this.sl.Clear();
+            }
+        }
+
+        public ICollection<T> this[int position]
+        {
+            get
+            {
+                lock (sl)
+                {
+                    return sl.ElementAt(position).Value;
+                }
+            }
+        }
+
+        public ICollection<T> First()
+        {
+            lock (sl)
+            {
+                return sl.First().Value;
+            }
+        }
+
+        public void Add(T key)
+        {
+            lock(sl)
+            {
+                bool inserted = false;
+                int compareVal;
+                SortedList<int, T[]> newsl = sl, newnewsl = new SortedList<int,T[]>();
+                KeyValuePair<int, T[]> newkvp;
+                List<T> ts;
+                T t;
+                foreach (KeyValuePair<int, T[]> kvp in sl)
+                {
+                    t = kvp.Value[0];
+                    compareVal = t.CompareTo(key);
+                    switch (compareVal)
+                    {
+                        case -1:
+                            newkvp = new KeyValuePair<int, T[]>(kvp.Key, new T[] { key });
+                            newsl.Remove(kvp.Key);
+                            newsl.Add(newkvp.Key, newkvp.Value);
+                            foreach(KeyValuePair<int, T[]> kv in newsl.Take(kvp.Key + 1))
+                            {
+                                newnewsl.Add(kv.Key, kv.Value);
+                            }
+                            newsl = newnewsl;
+                            newsl.Add(kvp.Key + 1, kvp.Value);
+                            for (int i = kvp.Key + 1; i < sl.Count; i--)
+                                newsl.Add(i + 1, sl[i]);
+
+                            inserted = true;
+                            break;
+                        case 1:
+                            break;
+                        case 0:
+                            ts = kvp.Value.ToList();
+                            ts.Add(key);
+                            newsl.Remove(kvp.Key);
+                            newsl.Add(kvp.Key, ts.ToArray());
+
+                            inserted = true;
+                            break;
+                    }
+
+                    if (inserted)
+                        break;
+                }
+                if (!inserted)
+                    sl.Add(sl.Count, new T[] { key });
+                Count++;
+                if (sl.Count > 0)
+                    sl = newsl;
+                else
+                    sl.Add(0, new T[]{ key });
+            }
+        }
+
+        public bool Remove(T key)
+        {
+            lock (sl)
+            {
+                bool res = false;
+                KeyValuePair<int, T[]> newkvp = new KeyValuePair<int,T[]>();
+                List<T> ts;
+                int indexOfRemoval = -1;
+                foreach (KeyValuePair<int, T[]> kvp in sl)
+                {
+                    if (kvp.Value.Contains(key))
+                    {
+                        ts = kvp.Value.ToList();
+                        ts.Remove(key);
+                        newkvp = new KeyValuePair<int,T[]>(kvp.Key, ts.ToArray());
+                        res = true;
+                        indexOfRemoval = kvp.Key;
+                    }
+                }
+
+                if (res == true)
+                {
+                    sl.Remove(indexOfRemoval);
+                    if (newkvp.Value.Count() > 0)
+                        sl.Add(newkvp.Key, newkvp.Value);
+                    Count--;
+                }
+
+                return res;
+            }
+        }
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            SortableListEnumer<T> enumer;
+            lock (sl)
+            {
+                enumer = new SortableListEnumer<T>(this);
+            }
+
+            return enumer;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        private class SortableListEnumer<TS> : IEnumerator<TS> where TS : IComparable
+        {
+            private int cur = -1;
+            private TS[] data;
+
+            public SortableListEnumer(SortableList<TS> sortlist)
+            {
+                data = sortlist.sl.SelectMany(kv => kv.Value).ToArray();
+            }
+
+            public void Dispose()
+            {
+
+            }
+
+            object IEnumerator.Current
+            {
+                get { return this.Current; }
+            }
+
+            public bool MoveNext()
+            {
+                cur++;
+                return cur < data.Length;
+            }
+
+            public void Reset()
+            {
+                this.cur = -1;
+            }
+
+            public TS Current
+            {
+                get { return data[cur]; }
+            }
+        }
+    }
+}
---
+++ JSLibrary/JSLibrary_Test/AndTest.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using JSLibrary.Data;
+
+namespace JSLibrary_Test
+{
+    [TestFixture]
+    public class AndTest
+    {
+        stuff s0, s1, s2, s3, s4;
+        SortableList<stuff> sl;
+        [SetUp]
+        public void Initialization()        
+        {        
+            sl = new SortableList<stuff>();            
+        }
+
+        [Test]
+        public void Add_CorrectOrder_Success()
+        {
+            sl.Add(s0 = new stuff(0));
+            sl.Add(s1 = new stuff(1));
+            sl.Add(s2 = new stuff(2));
+            sl.Add(s3 = new stuff(3));
+            sl.Add(s4 = new stuff(4));
+
+            Assert.AreEqual(5, sl.Count);
+
+            Assert.AreEqual(s0, sl[0].First());
+            Assert.AreEqual(s1, sl[1].First());
+            Assert.AreEqual(s2, sl[2].First());
+            Assert.AreEqual(s3, sl[3].First());
+            Assert.AreEqual(s4, sl[4].First());
+        }
+
+        [Test]
+        public void Add_WrongOrder_Success()
+        {
+            sl.Add(s2 = new stuff(2));
+            sl.Add(s0 = new stuff(0));
+            sl.Add(s1 = new stuff(1));
+            sl.Add(s4 = new stuff(4));
+            sl.Add(s3 = new stuff(3));
+
+            Assert.AreEqual(5, sl.Count);
+
+            Assert.AreEqual(s0, sl[0].First());
+            Assert.AreEqual(s1, sl[1].First());
+            Assert.AreEqual(s2, sl[2].First());
+            Assert.AreEqual(s3, sl[3].First());
+            Assert.AreEqual(s4, sl[4].First());
+        }
+
+        [Test]
+        public void Add_DuplicateSortValue_Success()
+        {
+            sl.Add(s2 = new stuff(2));
+            sl.Add(s0 = new stuff(0));
+            sl.Add(s1 = new stuff(1));
+            sl.Add(s3 = new stuff(2));
+
+            Assert.AreEqual(4, sl.Count);
+
+            Assert.AreEqual(s0, sl[0].First());
+            Assert.AreEqual(s1, sl[1].First());
+            Assert.AreEqual(s2, sl[2].First());
+            Assert.AreEqual(s3, sl[2].Last());
+        }
+
+        [Test]
+        public void Remove_NoDuplicate_Success()
+        {
+            sl.Add(s2 = new stuff(2));
+            sl.Add(s0 = new stuff(0));
+            sl.Add(s1 = new stuff(1));
+            sl.Add(s4 = new stuff(4));
+            sl.Add(s3 = new stuff(3));
+
+            sl.Remove(s3);
+
+            Assert.AreEqual(4, sl.Count);
+
+            Assert.AreEqual(s0, sl[0].First());
+            Assert.AreEqual(s1, sl[1].First());
+            Assert.AreEqual(s2, sl[2].First());
+            Assert.AreEqual(s4, sl[3].First());
+        }
+
+        [Test]
+        public void Remove_DuplicateExists_Success()
+        {
+            sl.Add(s0 = new stuff(0));
+            sl.Add(s1 = new stuff(0));
+            sl.Add(s2 = new stuff(1));
+            sl.Add(s3 = new stuff(2));
+            sl.Add(s4 = new stuff(3));
+            Assert.AreEqual(5, sl.Count);
+            Assert.AreEqual(2, sl[0].Count);
+
+            sl.Remove(s1);
+
+            Assert.AreEqual(4, sl.Count);
+
+            Assert.AreEqual(s0, sl[0].First());
+            Assert.AreEqual(1, sl[0].Count);
+            Assert.AreEqual(s2, sl[1].First());
+            Assert.AreEqual(s3, sl[2].First());
+            Assert.AreEqual(s4, sl[3].First());
+        }
+
+
+        [Test]
+        public void Enumeration_BothDuplicatesAndNoDuplicates_Success()
+        {
+            sl.Add(s0 = new stuff(0));
+            sl.Add(s1 = new stuff(0));
+            sl.Add(s2 = new stuff(1));
+            sl.Add(s3 = new stuff(2));
+            sl.Add(s4 = new stuff(3));
+            int i = 0;
+
+            foreach (stuff s in sl)
+            {
+                if (i == 0)
+                    Assert.IsTrue(s.val == s0.val || s.val == s1.val);
+                else if (i == 1)
+                    Assert.IsTrue(s.val == s0.val || s.val == s1.val);
+                else if (i == 2)
+                    Assert.AreEqual(s, s2);
+                else if (i == 3)
+                    Assert.AreEqual(s, s3);
+                else if (i == 4)
+                    Assert.AreEqual(s, s4);
+
+                i++;
+            }
+
+            Assert.AreEqual(5, i);
+        }
+
+        private class stuff : IComparable
+        {
+            public stuff(int i)
+            {
+                val = i;
+            }
+            public int val;
+            int IComparable.CompareTo(object obj)
+            {
+                if (((stuff)obj).val < val)
+                    return -1;
+                else if (((stuff)obj).val > val)
+                    return 1;
+                else
+                    return 0;
+            }
+        }
+    }
+}
